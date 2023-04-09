434f8ab5ddaa0d8b2ccb26b720ce27e773eedf8a
Денис <den@MacBook-Pro-Denis.local>
2023-04-09 20:49:22 +0300

    add Task_002

---
+++ Homework/Task_002/002.py
@@ -0,0 +1,12 @@
+# Задача 2: Найдите сумму цифр трехзначного числа.
+# 123 -> 6 (1 + 2 + 3) 100 -> 1 (1 + 0 + 0)
+
+number = int(input("Введите трехзначное число: "))
+
+numberTree = number%10
+numberTwo = int(((number%100)/10)//1)
+numberOne = number//100
+
+sumNumbers = numberOne + numberTwo + numberTree
+
+print(sumNumbers)
