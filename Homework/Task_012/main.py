78750d6927c372a8986abb89f19de2f4a19f2ce0
Денис <den@MacBook-Pro-Denis.local>
2023-04-13 20:17:43 +0300

    add Task_012

--- Homework/Task_012/main.py
+++ Homework/Task_012/main.py
@@ -3,7 +3,21 @@
 # натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать. Для
 # этого Петя делает две подсказки. Он называет сумму этих чисел S и их
 # произведение P. Помогите Кате отгадать задуманные Петей числа.
-# 4 4 -> 2 2 
+# 4 4 -> 2 2
 # 5 6 -> 2 3
 
+import math
 
+numberOne = 168
+numberTwo = 10
+
+numberS = numberOne + numberTwo
+numberP = numberOne*numberTwo
+
+if numberOne > 1000 or numberTwo > 1000:
+    print('Введено неверное число')
+else:
+    x = (numberS - math.sqrt(numberS**2-4*numberP))/2
+    y = numberS - x
+    print(f'Если сумма чисел = {numberS}, а произведение = {numberP},')
+    print(f'то первое число = {int(x)}, а второе = {int(y)}')
