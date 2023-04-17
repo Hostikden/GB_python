6733fd5d05f77334ce62df0719018f02499f21f2
Hostik <Hostik.den@gmail.com>
2023-04-17 14:03:27 +0300

    fix README 1.4

---
+++ Sem/Sem_001/001.py
@@ -0,0 +1,7 @@
+username = input('Введите имя: ')
+if username == 'Маша':
+    print('Ура, это же МАША!')
+elif username == 'Марина':
+    print('Я так ждала вас, Марина...')
+else:
+    print("Привет, ", username)
