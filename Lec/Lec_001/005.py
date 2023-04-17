c6761ef0183231fbe91d0ed44dc9121054135f31
Hostik <Hostik.den@gmail.com>
2023-04-17 15:20:32 +0300

    rename folders

---
+++ Lec/Lec_001/005.py
@@ -0,0 +1,9 @@
+'''
+Пример range
+
+'''
+
+r = range(100, 0, -20)
+
+for i in r:
+    print(i)
--- Sem/Sem_001/002.py
+++
@@ -1,7 +0,0 @@
-n = 423
-summa = 0
-while n > 0:
-    x = n % 10
-    summa += x
-    n = n // 10
-print(summa)
